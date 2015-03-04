ce130c9822c42d5e84f735326af06d89b4fa0fe3
Aditya Bansod <hyperionab@yahoo.com>
2015-03-04 14:02:05 -0800

    inital commit

---
+++ public/js/_config.js
@@ -0,0 +1,9 @@
+/* Here you can configure Angelina */
+
+var percent_ready = 72, // Enter here value of readiness your website
+
+	SiteStartDate = '2015/07/4 08:05', // The Date Of Site Opening
+	
+	YoutubeBgCode = 'f4DY5poQDx8'; // The Part Of Youtube Video URL
+
+	

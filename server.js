a651d4c71946cdc7bdfce8b1561f37150c15077a
Aditya Bansod <hyperionab@yahoo.com>
2015-03-06 10:57:45 -0800

    more stuff

--- server.js
+++ server.js
@@ -1,9 +1,59 @@
-var express = require('express');
+var express = require('express'),
+    bodyparser = require('body-parser');
 var port = process.env.PORT || 5000;
+var sendgrid  = require('sendgrid')(process.env.SENDGRID_API_USER, process.env.SENDGRID_API_KEY);
+var Slackbot = require('slackbot')
+var slackbot = new Slackbot("lumahealth", "xoxb-3955566628-gP7poST27lB7lNaPbTSyvmEX");
+
 var app = express();
- 
+app.use(bodyparser.urlencoded());
+
 app.get('/', function(request, response) {
     response.sendfile(__dirname + '/public/index.html');
-}).configure(function() {
+});
+
+app.post('/signup', function(request, response) {
+
+	response.sendfile(__dirname + '/public/thankyou.html');
+
+	var name = request.body.name,
+		email = request.body.email,
+		role = request.body.role,
+		org = request.body.org;
+
+	var mailbody = 'New web form submission. \n';
+	mailbody += 'Name: ' + name + '\n';
+	mailbody += 'Email: ' + email + '\n';
+	mailbody += 'Role: ' + role + '\n';
+	mailbody += 'Org: ' + org + '\n';
+	mailbody += (new Date());
+
+	console.log(mailbody);
+
+	sendgrid.send({
+	  to:       'info@lumahealth.io',
+	  from:     'lh-marketing@lumahealth.io',
+	  subject:  'Web Form Submission ' + name,
+	  text:     mailbody
+	}, function(err, json) {
+	  if (err) { return console.error(err); }
+	  console.log(json);
+	});
+
+	slackbot.send('#general',
+		'sup fools. ' + name + ' signed up, check ur email for the deets',
+		function(err, res, body) {
+		  if(err) {
+		  	console.log(err);
+		  };
+		  console.log(body);
+	});
+
+
+});
+
+app.configure(function() {
     app.use('/', express.static(__dirname + '/public/'));
-}).listen(port);
+});
+
+app.listen(port);
